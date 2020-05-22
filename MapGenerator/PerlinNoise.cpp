4b5bdeaf7601c2d3fab03a1aea9782f9d82eeb5e
kubakakauko <kubakakauko@gmail.com>
2020-05-22 15:54:40 +0100

    No Changes

--- MapGenerator/PerlinNoise.cpp
+++ MapGenerator/PerlinNoise.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 #include <numeric>
 
-PerlinNoise::PerlinNoise()
+PerlinNoise::PerlinNoise() //Perlin Noise
 {
 	p = {
 		151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,
